4cc9ac0609b4156674e18775bf136b1e1c972c0c
sishenhei7 <982505136@qq.com>
2019-09-12 18:48:59 +0800

    chore: modify blogs

--- mock/blogs.js
+++ mock/blogs.js
@@ -6,33 +6,35 @@ const count = 100;
 for (let i = 0; i < count; i++) {
   List.push(Mock.mock({
     id: '@increment',
-    label: Mock.Random.cword(2, 5),
+    label: [
+      Mock.Random.cword(2, 5),
+      Mock.Random.cword(2, 5),
+      Mock.Random.cword(2, 5),
+    ],
     'type|1': ['zh', 'en', 'all'],
     author: '@cname',
     title: Mock.Random.csentence(3, 10),
+    isDraft: Mock.Random.boolean(),
     content: Mock.Random.csentence(100, 500),
     created_at: Mock.Random.date('yyyy-MM-dd'),
     updated_at: Mock.Random.date('yyyy-MM-dd'),
   }));
 }
 
-export default [
-  {
+export default [{
     url: '/blogs',
     type: 'get',
-    response: () => {
-      return {
-        ok: true,
-        data: List,
-      };
-    },
+    response: () => ({
+      ok: true,
+      data: List,
+    }),
   },
 
   {
     url: '/blogs/:id',
     type: 'get',
-    response: config => {
-      const id = Number(config.params.id);
+    response: (req) => {
+      const id = Number(req.params.id);
       const filteredList = List.filter(item => item.id === id);
       return {
         ok: true,
@@ -44,10 +46,17 @@ export default [
   {
     url: '/blogs',
     type: 'post',
-    response: config => {
-      const lastOne = List(List.length - 1);
+    response: (req) => {
+      const lastOne = List[List.length - 1];
+      const parsedConfig = {
+        title: req.body.title,
+        label: JSON.parse(req.body.label),
+        type: req.body.type,
+        content: req.body.content,
+        isDraft: !!req.body.isDraft,
+      };
       const item = {
-        ...config.body.params,
+        ...parsedConfig,
         id: lastOne.id + 1,
         created_at: Mock.Random.date('yyyy-MM-dd'),
         updated_at: Mock.Random.date('yyyy-MM-dd'),
@@ -63,13 +72,26 @@ export default [
   {
     url: '/blogs/:id',
     type: 'put',
-    response: config => {
-      const id = Number(config.params.id);
+    response: (req) => {
+      const id = Number(req.params.id);
       const index = List.findIndex(item => item.id === id);
+
+      const parsedConfig = {};
+      for (let item in req.body) {
+        if (item === 'label') {
+          parsedConfig[item] = JSON.parse(req.body[item]);
+        } else if (['title', 'type', 'content'].includes(item)) {
+          parsedConfig[item] = req.body[item];
+        } else if (item === 'isDraft') {
+          parsedConfig[item] = !!req.body[item];
+        }
+      }
+
       List[index] = {
         ...List[index],
-        ...config.body.params,
+        ...parsedConfig,
       };
+
       return {
         ok: true,
         data: List,
@@ -78,10 +100,25 @@ export default [
   },
 
   {
+    url: '/blogs/:id/publish',
+    type: 'put',
+    response: (req) => {
+      const id = Number(req.params.id);
+      const index = List.findIndex(item => item.id === id);
+      List[index].isDraft = false;
+      return {
+        ok: true,
+        data: List,
+        index: index,
+      };
+    },
+  },
+
+  {
     url: '/blogs/:id',
     type: 'delete',
-    response: config => {
-      const id = Number(config.params.id);
+    response: (req) => {
+      const id = Number(req.params.id);
       const index = List.findIndex(item => item.id === id);
       List.splice(index, 1);
       return {
@@ -91,5 +128,3 @@ export default [
     },
   },
 ];
-
-
